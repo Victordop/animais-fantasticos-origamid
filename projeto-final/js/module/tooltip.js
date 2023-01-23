543173e6a81864a6cc9f95ca6e6125ac125ece72
victorop <victordopr@gmail.com>
2023-01-23 10:29:16 -0300

    comecei refatorar a feature tooltip

--- projeto-final/js/module/tooltip.js
+++ projeto-final/js/module/tooltip.js
@@ -1,55 +1,46 @@
-export default function initToolTip() {
-  const toolTips = document.querySelectorAll('[data-tooltip]');
-
-  function criaDivToolTip(elemento) {
-    const caixaToolTip = document.createElement('div');
-    caixaToolTip.classList.add('tooltip');
-    const texto = elemento.getAttribute('aria-label'); // o texto que está lá no html, funciona para leitor
-    // mas n aparece como tooltip
-    caixaToolTip.innerText = texto;
-    document.body.appendChild(caixaToolTip);
-    return caixaToolTip;
+export default class Tooltip {
+  constructor(tooltip) {
+    this.tooltips = document.querySelectorAll(tooltip);
   }
 
-  function funcMouseOver(event) {
-    const toolTip = criaDivToolTip(this);
-    // toolTip.style.top = event.pageY + "px"; //aqui dá um valor inteiro, temos que somar com px para funcionar
-    // toolTip.style.left = event.pageX + "px";
 
-    // console.log(toolTip.style)//mostra um objeto style com os estilos que posso alterar
-    // console.log(this) //this é o item!!!
-    // console.log(event) //para gente ver o posicionamento do elemento (page x, page y)
-
-    const onMouseMove = {
-      toolTip: '',
-      handleEvent(event) {
-        toolTip.style.top = `${event.pageY + 20}px`; // aqui dá um valor inteiro, temos que somar com px para funcionar
-        toolTip.style.left = `${event.pageX + 20}px`; // o 20 é pra desgrudar do ponteiro e n ficar piscando
-      },
-    };
-
-    onMouseMove.toolTip = toolTip;
-    this.addEventListener('mousemove', onMouseMove);
-
-    const removeTip = {
-      element: '',
-      toolTip: '',
-      handleEvent() { // ativa o método quando passamos no eventListener como uma função de callback
-        this.toolTip.remove(); // remove o elemento do Dom; //this no caso é o toolTip que está aqui
-        this.element.removeEventListener('mouseleave', removeTip); // usei pra remover o eventListener
-        this.element.removeEventListener('mousemove', onMouseMove);
-      },
-    };
-    removeTip.element = this; // o this é o item
-    removeTip.toolTip = toolTip; // setei o valor da propriedade toolTip do objeto
-
-    this.addEventListener('mouseleave', removeTip); 
+  addTooltipsEvent(){
+    this.tooltips.forEach((item) => {
+      item.addEventListener('mouseover',this.onMouseOver);
+    })
+  
   }
 
-  toolTips.forEach((item) => {
-    item.addEventListener('mouseover', funcMouseOver);
-  });
+  init(){ //continuar daqui
 
-  // funçao cria a div, estiliza ela, adiciona texto e coloca na página
-}
+  }
 
+
+  onMouseOver(event) {
+this.currentTarget.criarTooltipBox(event.currentTarget);
+
+  event.currentTarget.addEventListener('mousemove',this.onMouseMove);
+  event.currentTarget.addEventListener('mouseleave', this.onMouseLeave);
+  }
+
+ 
+    onMouseLeave(){
+      this.tooltipBox.remove();
+      this.element.removeEventListener('mouseleave', onMouseLeave);
+      this.element.removeEventListener('mousemove', onMouseMove);
+    }
+
+
+    onMouseMove(event) {
+      this.tooltipBox.style.top = event.pageY + 20 + 'px';
+      this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    }
+
+   criarTooltipBox(element) {
+    const tooltipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    tooltipBox.classList.add('tooltip');
+    tooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+    this.tooltipBox=tooltipBox;
+  }
