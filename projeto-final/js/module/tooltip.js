8fbc28d1f96ae741349c42d2bb12e1ae55655908
victorop <victordopr@gmail.com>
2023-02-01 08:09:40 -0300

    conclui a refatoração da função em classe

--- projeto-final/js/module/tooltip.js
+++ projeto-final/js/module/tooltip.js
@@ -8,43 +8,6 @@ export default class Tooltip {
     this.onMouseLeave = this.onMouseLeave.bind(this);
   }
 
-  addTooltipsEvent() {
-    this.tooltips.forEach((item) => {
-      item.addEventListener('mouseover', this.onMouseOver);
-    });
-  }
-
-  init() {
-    if (this.tooltips.length) {
-      this.addTooltipsEvent();
-    }
-    return this;
-  }
-
-  // ao passar o mouse por cima do mapa
-  onMouseOver({ currentTarget }) {
-    this.criarTooltipBox(currentTarget);
-    currentTarget.addEventListener('mousemove', this.onMouseMove);
-    currentTarget.addEventListener('mouseleave', this.onMouseLeave);
-  }
-
-  onMouseLeave({ currentTarget }) { // desetruturei o event pois estou usando só currentTarget
-    this.tooltipBox.remove();
-    currentTarget.removeEventListener('mouseleave', this.onMouseLeave);
-    currentTarget.removeEventListener('mousemove', this.onMouseMove);
-  }
-
-  onMouseMove(event) {
-    this.tooltipBox.style.top = `${event.pageY + 20}px`;
-    // console.log(event.pageX, window.innerWidth);// event.pageX => onde o evento está acontecendo no eixo x, window.innerWidth => largura da janela
-    if (event.pageX > window.innerWidth) {
-      this.tooltipBox.style.left = `${event.pageX - 10} px`;
-    } else {
-      this.tooltipBox.style.left = `${event.pageX + 20} px`;
-    }
-    console.log(event.pageX, window.innerWidth); //parei aqui corrigir esse bug do recuo da tooltip
-  }
-
   // cria tooltip e adiciona no body
   criarTooltipBox(element) {
     const tooltipBox = document.createElement('div');
@@ -53,5 +16,41 @@ export default class Tooltip {
     tooltipBox.innerText = text;
     document.body.appendChild(tooltipBox);
     this.tooltipBox = tooltipBox;
+  }
+
+  addTooltipsEvent() {
+    this.tooltips.forEach((item) => {
+      item.addEventListener('mouseover', this.onMouseOver);
+    });
+  }
+
+  // ao passar o mouse por cima do mapa cria a tooltip, adiciona evento de mousemove e mouseleave
+  onMouseOver({ currentTarget }) {
+    this.criarTooltipBox(currentTarget);
+    currentTarget.addEventListener('mousemove', this.onMouseMove);
+    currentTarget.addEventListener('mouseleave', this.onMouseLeave);
+  }
+
+  onMouseMove(event) {
+    this.tooltipBox.style.top = `${event.pageY + 20}px`;
+    // console.log(event.pageX, window.innerWidth);// event.pageX => onde o evento está acontecendo no eixo x, window.innerWidth => largura da janela
+    if (event.pageX + 452 > window.innerWidth) {
+      this.tooltipBox.style.left = `${event.pageX - 200}px`;
+    } else {
+      this.tooltipBox.style.left = `${event.pageX + 50}px`;
+    }
+  }
+
+  onMouseLeave({ currentTarget }) { // desetruturei o event pois estou usando só currentTarget
+    this.tooltipBox.remove();
+    currentTarget.removeEventListener('mouseleave', this.onMouseLeave);
+    currentTarget.removeEventListener('mousemove', this.onMouseMove);
+  }
+
+  init() {
+    if (this.tooltips.length) {
+      this.addTooltipsEvent();
+    }
+    return this;
   }
 }
