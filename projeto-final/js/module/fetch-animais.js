5c79e249fc349c58448702b99c333f69b5eaba82
victorop <victordopr@gmail.com>
2023-02-02 11:00:23 -0300

    comecei refatorar a função fetchAnimais

--- projeto-final/js/module/fetch-animais.js
+++ projeto-final/js/module/fetch-animais.js
@@ -1,13 +1,12 @@
 import AnimaNumeros from './anima-numeros.js'; // problema: a animação dos números estava acontecendo antes do fetch
 // solução importar o module para cá ao invés de importar pro script
 
-export default function fetchAnimais() {
-  async function requisitaAnimais(url) {
+export default function fetchAnimais(url, target) {
+  async function requisitaAnimais() {
     const animaisResponse = await fetch(url);
     const animaisJSON = await animaisResponse.json(); // retorna um array com objetos
 
-    // console.log(animaisJSON);
-    const secaoNumeroAnimais = document.querySelector('.numeros-grid');
+    const secaoNumeroAnimais = document.querySelector(target);
 
     function createAnimal(animal) { // estou recriando a parte do html pelo script
       // console.log(animal)
@@ -28,7 +27,7 @@ export default function fetchAnimais() {
     });
   }
 
-  requisitaAnimais('./numeroAnimaisApi.json');
+  requisitaAnimais(url);
 }
 
 // vai fazer fetch podemos usar then ou async/await
