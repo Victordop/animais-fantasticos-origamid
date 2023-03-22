852cec227bf2756ec976441b81b394a70c907146
victorop <victordopr@gmail.com>
2023-03-22 09:56:42 -0300

    utilizei a função debounce para reduzir o número de eventos ao scrollar

---
+++ projeto-final/js/module/debounce.js
@@ -0,0 +1,15 @@
+export default function debounce(callback, delay) {
+  let timer;
+
+  return (...args) => { // com esta desestruturação, posso passar argumentos na minha função de callback
+    if (timer) clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      callback(...args); // faz parte da desestrutuação acima
+      timer = null;
+    }, delay);
+  };
+}
+
+// Debounce serve para o evento não ficar sendo ativado alucinadamente, ele filtra o número
+// de vezes que o evento é ativado
